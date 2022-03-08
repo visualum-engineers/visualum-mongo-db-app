f7bad3382677dbbad0b6eb4bc3d9d215214f6738
Arky Asmal <arkyasmal@gmail.com>
2022-03-08 05:25:04 -0500

    "added creating new org document"

--- functions/user/create_user.js
+++ functions/user/create_user.js
@@ -2,13 +2,12 @@ exports = async function ({
   email = "",
   email_verified = false,
   account_type = "",
-  additional_data = {}
+  additional_data = {},
 }) {
   //user collection
-  const collection = context.services
-    .get("mongodb-atlas")
-    .db("Development")
-    .collection("users");
+  const database = context.services.get("mongodb-atlas").db("Development");
+
+  const collection = database.collection("users");
   const user_id = context.user.id;
   const name = context.user.data.name;
   let newUserDocument;
@@ -30,7 +29,13 @@ exports = async function ({
     };
     //we must create an organization document
     if (account_type === "admin") {
-
+      const org_collection = database.collection("organizations");
+      const org_data = {
+        ...additional_data.org_data,
+        _id: new BSON.ObjectId()
+      };
+      const org_id = await org_collection.insertOne(org_data);
+      newUserDocument.organization_id = org_id;
     }
   } catch (e) {
     const errorParms = {
@@ -49,7 +54,8 @@ exports = async function ({
     };
   } catch (e) {
     const errorParms = {
-      error_message: "could not create a new user. Invalid information, or user already exists",
+      error_message:
+        "could not create a new user. Invalid information, or user already exists",
       error_metadata: e,
     };
     throw context.functions.execute("create_async_error", errorParms);
